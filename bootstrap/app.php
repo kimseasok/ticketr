fcf02643de59c19206ff73a15c9f6d29bf1baa3a
Kimsea Sok <basicblogtalk@gmail.com>
2025-09-30 19:59:23 +0700

    fix: restore controller authorization support

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,9 +4,10 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-return Application::configure(basePath: dirname(__DIR__))
+$app = Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
@@ -15,4 +16,15 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
-    })->create();
+    })
+    ->create();
+
+if (! file_exists($app->environmentFilePath())) {
+    $testingEnvironment = $app->environmentPath().'/.env.testing';
+
+    if (file_exists($testingEnvironment)) {
+        $app->loadEnvironmentFrom('.env.testing');
+    }
+}
+
+return $app;
